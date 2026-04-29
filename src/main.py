b4e8447d5f92eece2815d7ff34f0f09bb3ce751e
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-04-29 20:36:49 +0530

    remove preferences menu btn

--- src/main.py
+++ src/main.py
@@ -20,7 +20,6 @@ class DefuseApplication(Adw.Application):
         )
         self.create_action("quit", lambda *_: self.quit(), ["<control>q"])
         self.create_action("about", self.on_about_action)
-        self.create_action("preferences", self.on_preferences_action)
 
     def do_activate(self):
         """Called when the application is activated.
@@ -46,10 +45,6 @@ class DefuseApplication(Adw.Application):
         # Translators: Replace "translator-credits" with your name/username, and optionally an email or URL.
         about.set_translator_credits(_("translator-credits"))
         about.present(self.props.active_window)
-
-    def on_preferences_action(self, widget, _):
-        """Callback for the app.preferences action."""
-        print("app.preferences action activated")
 
     def create_action(self, name, callback, shortcuts=None):
         """Add an application action.
