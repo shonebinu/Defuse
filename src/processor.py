6f3b1ac629e39f1847c278b6df891770d08d82d1
Shone Binu <62597277+shonebinu@users.noreply.github.com>
2026-05-04 18:25:39 +0530

    fix: fallback to cpu provider if webgpu fails

--- src/processor.py
+++ src/processor.py
@@ -1,5 +1,5 @@
 import io
-from typing import List
+from typing import List, Optional
 
 import numpy as np
 import onnxruntime as ort
@@ -12,14 +12,8 @@ class ImageProcessor:
     )
 
     def __init__(self):
-        self.onnx_session = None
-
-    def ensure_session(self):
-        if self.onnx_session is None:
-            # webgpu provider on x86 and cpu on aarch64
-            self.onnx_session = ort.InferenceSession(
-                self.isnet_model_path, providers=ort.get_available_providers()
-            )
+        self.onnx_session: Optional[ort.InferenceSession] = None
+        self.current_provider: Optional[str] = None
 
     def get_supported_mimes(self) -> List[str]:
         exts = Image.registered_extensions()
@@ -27,9 +21,11 @@ class ImageProcessor:
 
         return [f"image/{ext[1:]}" for ext in supported_extensions]
 
-    def remove_bg(self, img_bytes: bytes, output_format="PNG") -> bytes:
+    def run_model(self, img_bytes: bytes, output_format: str) -> bytes:
+        if self.onnx_session is None:
+            raise RuntimeError("ONNX session is not initialized.")
+
         # https://github.com/danielgatis/rembg/blob/main/rembg/sessions/dis_general_use.py
-        self.ensure_session()
 
         img = ImageOps.exif_transpose(Image.open(io.BytesIO(img_bytes)).convert("RGB"))
 
@@ -38,7 +34,7 @@ class ImageProcessor:
         arr = np.array(img.resize(size, Image.Resampling.LANCZOS)).astype(np.float32)
         img_input = ((arr / max(arr.max(), 1e-6) - mean) / std).transpose(2, 0, 1)
 
-        out = self.onnx_session.run(  # type: ignore
+        out = self.onnx_session.run(
             None,
             {self.onnx_session.get_inputs()[0].name: img_input[None]},  # type: ignore
         )[0][0, 0]
@@ -53,3 +49,26 @@ class ImageProcessor:
             bio, format=output_format
         )
         return bio.getvalue()
+
+    def remove_bg(self, img_bytes: bytes, output_format="PNG") -> bytes:
+        if not self.onnx_session:
+            # webgpu(default) and cpu in x64 and cpu only in arm
+            self.onnx_session = ort.InferenceSession(
+                self.isnet_model_path, providers=ort.get_available_providers()
+            )
+            self.current_provider = self.onnx_session.get_providers()[0]
+
+        try:
+            return self.run_model(img_bytes, output_format)
+        except Exception:
+            if self.current_provider == "CPUExecutionProvider":
+                raise
+
+            print(f"Falling back to CPUExecutionProvider from {self.current_provider}")
+
+            self.current_provider = "CPUExecutionProvider"
+            self.onnx_session = ort.InferenceSession(
+                self.isnet_model_path, providers=["CPUExecutionProvider"]
+            )
+
+            return self.run_model(img_bytes, output_format)
